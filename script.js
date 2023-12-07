cc65fcb9b20c47ade12aa44ce8e7c1770278c7ff
Adam Welin <welinadam@users.noreply.github.com>
2023-12-07 10:30:45 +0100

    Slider for images on mobile movie page

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ collapsibleTitles.forEach((title) => {
     content.classList.toggle("expanded");
   });
 });
+
+// Slider arrow buttons
+const filmStillsArrowRight = document.querySelector(
+  ".column-images .arrow.right"
+);
+const filmStillsArrowLeft = document.querySelector(
+  ".column-images .arrow.left"
+);
+
+filmStillsArrowRight.addEventListener("click", () => {
+  // Viewport width
+  const viewportWidth = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+
+  document.querySelector(".column-images .slider-slides").scrollLeft +=
+    viewportWidth;
+});
+
+filmStillsArrowLeft.addEventListener("click", () => {
+  // Viewport width
+  const viewportWidth = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+
+  document.querySelector(".column-images .slider-slides").scrollLeft -=
+    viewportWidth;
+});
