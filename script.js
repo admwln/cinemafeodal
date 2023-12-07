65c1231c872ae798befe19980263fca211dc9a2b
Adam Welin <welinadam@users.noreply.github.com>
2023-12-07 14:19:11 +0100

    Title-booking-card complete desktop

--- script.js
+++ script.js
@@ -64,3 +64,14 @@ collapsibleTitles.forEach((title) => {
     }
   });
 });
+
+// Mouse over .book-btns
+const bookBtns = document.querySelectorAll(".book-btn");
+bookBtns.forEach((btn) => {
+  btn.addEventListener("mouseover", () => {
+    btn.classList.add("hover");
+  });
+  btn.addEventListener("mouseout", () => {
+    btn.classList.remove("hover");
+  });
+});
