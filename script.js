58fa25a837722fde50d89117e806a9e7f7b7ec6d
Jonas Pettersson <jonas.pettersson88@gmail.com>
2023-12-11 16:07:24 +0100

    added next month section

--- script.js
+++ script.js
@@ -11,8 +11,31 @@ function scrollMovieCards(direction) {
 }
 
 // Function to scroll movie cards
+function scrollMovieCardsNm(direction) {
+    const container = document.getElementById('movieCardContainerNm');
+    const scrollAmount = 300;
+
+    if (direction === 'left') {
+        container.scrollLeft -= scrollAmount;
+    } else if (direction === 'right') {
+        container.scrollLeft += scrollAmount;
+    }
+}
+
+// Function to scroll movie cards
 function scrollMovieCardsMob(direction) {
     const container = document.getElementById('movieCardContainerMob');
+    const scrollAmount = 300;
+
+    if (direction === 'left') {
+        container.scrollLeft -= scrollAmount;
+    } else if (direction === 'right') {
+        container.scrollLeft += scrollAmount;
+    }
+}
+
+function scrollMovieCardsMobNm(direction) {
+    const container = document.getElementById('movieCardContainerMobNm');
     const scrollAmount = 300;
 
     if (direction === 'left') {
